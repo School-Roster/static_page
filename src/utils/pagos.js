46777b1e11665bcdb5f74379cebbd4df0cda28fa
Adrian Botello <adrianbotellog01@gmail.com>
2025-04-07 13:47:24 -0600

    update: pagos

--- src/utils/pagos.js
+++ src/utils/pagos.js
@@ -1,3 +1,32 @@
-function async getPagos() {
-    const response = await fetch('');
+async function getPagos() {
+    const response = await fetch('http://localhost:3000/create-checkout-session');
 }
+
+
+// const Plus = {
+//     currency: "mxn",
+//     amount: 99,
+//     quantity: 1,
+//     productName: "Membresia Plus",
+// }
+
+// async function getText() {
+//     try {
+//         const response = await fetch('http://localhost:3000/create-checkout-session', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(Plus)
+//         })
+//         const data = await response.json()
+        
+//         if(data.url) {
+//             window.location.href = data.url
+//         } else {
+//             console.log("No url found")
+//         }
+//     } catch (error) {
+//         console.log("Error: ", error.message || error)
+//     }
+// }
